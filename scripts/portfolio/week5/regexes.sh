6dd0716e6bca94ab5da5d09cf9e49947ee18c7ea
Jody Petroni <jpetroni@our.ecu.edu.au>
2021-06-17 08:01:20 +1000

    week 5

--- scripts/portfolio/week5/regexes.sh
+++ scripts/portfolio/week5/regexes.sh
@@ -1,7 +1,12 @@
-path="~/student/scripts/portfolio/"
-#all sed statements
-grep -r 'sed '  ~/student/
-#all lines that start with the letter m
-#all line that contain 3 digit numbers
-#all echo statements with at least 3 words
-#all lines that would make a good password
+#!/bin/bash
+cd ~/student/scripts/portfolio/
+echo "all sed statements"
+grep -r 'sed ' 
+echo "all lines that start with the letter m"
+grep -r '^m'
+echo "all line that contain 3 digit number"
+grep -Er '\b[0-9]{3}\b' 
+echo "all echo statements with at least 3 words"
+grep -Er 'echo "(\w+\b.){3,}$'
+echo "all lines that would make a good password"
+grep -Er '^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}'
